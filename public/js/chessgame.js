73ee08d6731bcee376e3ae3dd294f05729c6a7cb
░░░░░U░Z░A░I░R░░░░ <uzairshekhani2017@gmail.com>
2024-06-08 18:18:53 +0500

    draggable function

--- public/js/chessgame.js
+++ public/js/chessgame.js
@@ -1,5 +1,3 @@
-const { Chess } = require("chess.js");
-
 const socket = io();
 const chess = new Chess()
 const boardElement = document.querySelector(".chessboard")
@@ -9,9 +7,37 @@ let sourceSquare = null;
 let playerRole = null;
 
 const renderBoard = () => {
-    const
-}
+    const board = chess.board();
+    boardElement.innerHTML = "";
+    board.forEach((row, rowindex) => {
+        row.forEach((square,squareindex) => {
+            const squareElement = document.createElement("div")
+            squareElement.classList.add("square",
+                (rowindex + squareindex)%2 === 0 ? "light" : "dark"
+            );
+
+            squareElement.dataset.row = rowindex
+            squareElement.dataset.col = squareindex
+
+            if(square){
+                const pieceElement = document.createElement("div")
+                pieceElement.classList.add("piece", square.color === "w" ? "white" : "black")
+            };
+            pieceElement.innerText = ""
+            pieceElement.draggable = playerRole === square.color
+
+            pieceElement.addEventListener("dragstart", () => {
+                if(pieceElement.draggable){
+                    draggedPiece = pieceElement;
+                    sourceSquare = {row : rowindex, col : squareindex}
+                }
+            })
+        });
+    });
+};
 
 const handleMove = () => {}
 
 const getPieceUnicode = () => {}
+
+renderBoard()
